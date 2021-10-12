f1577b40daf277c1f6b444f72530eb9a9de63c95
MacarenaPonce <macarena.ponce.pegueroles@gmail.com>
2021-10-12 12:06:52 -0700

    Se trabaja en ordenar

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 // estas funciones son de ejemplo
-
+//hay que agregar un import data
 export const example = () => {
   return 'example';
 };
@@ -7,3 +7,24 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//Ordenar año de publicación, alfabeticamente y por rating score)
+
+/* export const yearMovie = (arraytosort) => {
+  return arraytosort.sort(function(a, b) {
+    return compareStrings(a.release_date, b.release_date);
+})
+};
+
+export const ordenAlfabetico = (arraytoSort) => {
+  return arraytoSort.sort(function(a, b){
+    return compareStrings (a.title, b.title)
+  })
+}
+
+//Filtrar cosas? por director quiza
+export const filterByDirector = (arraytoFilter, filterValue) => {
+  return arraytoFilter.filter((film) => film.director === filterValue);
+  }; */
+
+//funcion
